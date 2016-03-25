38061f0976c5fa94daaf991db08cda40ffd090b8
Sorongoku <gonzalorpg@gmail.com>
2016-03-25 16:31:13 -0300

    Update kaioKen.cpp

--- algo3-tp1/src/kaioKen.cpp
+++ algo3-tp1/src/kaioKen.cpp
@@ -2,6 +2,47 @@
 
 void kaioKen (int cantGuerreros) {
 
+	int divisor = cantGuerreros;
+	int pelea = 1;
+
+	cout << ceil(log2(cantGuerreros)) << endl;
+
+	while (divisor!= 1){
+		if(divisor % 2 == 1){
+			divisor = (divisor / 2) + 1;
+		}else{
+			divisor = (divisor / 2);
+		}
+
+		int j = 1;
+		int i = 1;
+
+		while (i<= (cantGuerreros / 2)){
+			if(i>j*divisor)
+				j++;
+			if(j%2 == 0){
+				cout << "B(" << pelea << "2) ";
+			}else{
+				cout << "B(" << pelea << "1) ";
+			}
+			i++;
+		}
+        j = 1;
+		while (i > (cantGuerreros / 2) && i <= cantGuerreros){
+			if(i> j*divisor + cantGuerreros/2 )
+				j++;
+			if(j%2 == 0){
+				cout << "B(" << pelea << "1) ";
+			}else{
+				cout << "B(" << pelea << "2) ";
+			}
+			i++;
+		}
+
+		pelea++;
+		cout <<endl;
+	}
+
 }
 
 
