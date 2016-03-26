fd67f6c5dee0d0823234997fdebab93fcd9ee6ab
Leandro Motta <leamotta@msn.com>
2016-03-25 23:20:01 -0300

    genkidama 1.0

--- algo3-tp1/src/genkidama.cpp
+++ algo3-tp1/src/genkidama.cpp
@@ -1,4 +1,41 @@
+#include "genkidama.h"
 
+void genkidama(int N,int T,vector<pair<int,int> > enemigos){
+	int genkidamas=0;
+	int i=0;
+	list<int> disparados;
+	while (i<N) {
+		int disparado=masLejanoQueMeMata(i,N,T,enemigos);
+	    i = masLejanoQueMato(disparado,N,T,enemigos) + 1;
 
+	    disparados.push_back(disparado+1); //el 0 de la lista es el enemigo 1 y asi sucesivamente
+	    genkidamas++;
+	}
 
+	printf("%d\n",genkidamas);
+	for(int i=0; i<genkidamas; i++){
+	  	printf("%d ",disparados.front());
+		disparados.pop_front();
+	}
+}
+
+int masLejanoQueMeMata(int aMatar,int N, int T,vector<pair<int,int> > enemigos){
+    int j=aMatar+1;
+    while(loMata(j,aMatar,N,T,enemigos)){//me mata el siguiente?
+      j++;
+    }
+    return j-1;//devuelvo ultimo que me mata
+}
+
+int masLejanoQueMato(int disparado, int N,int T, vector<pair<int,int> > enemigos){
+  int j=disparado+1;
+  while(loMata(disparado,j,N,T,enemigos)){//mato al siguiente?
+    j++;
+  }
+  return j-1;//devuelvo ultimo que mato
+}
+
+bool loMata(int i,int j,int N,int T,vector<pair<int,int> > enemigos){//mata i al j?
+  return enemigos[i].first + T >= enemigos[j].first && enemigos[i].second + T >= enemigos[j].second;
+}
 
--- algo3-tp1/src/genkidama.h
+++ algo3-tp1/src/genkidama.h
@@ -1,8 +1,14 @@
 #ifndef GENKIDAMA_H_
 #define GENKIDAMA_H_
+#include <stdio.h>
+#include <math.h>
+#include <list>
+#include <vector>
+using namespace std;
 
-
-
-
+void genkidama(int N,int T,vector<pair<int,int> > enemigos);
+int masLejanoQueMato(int disparado,int N,int T,vector<pair<int,int> > enemigos);
+int masLejanoQueMeMata(int aMatar,int N,int T, vector<pair<int,int> > enemigos);
+bool loMata(int i,int j,int N,int T,vector<pair<int,int> > enemigos);
 
 #endif /* GENKIDAMA_H_ */
