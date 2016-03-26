79bb6428c9dc96ff5331ca95764006c69e720021
Sorongoku <gonzalorpg@gmail.com>
2016-03-26 01:22:58 -0300

    Update genkidama.cpp

--- algo3-tp1/src/genkidama.cpp
+++ algo3-tp1/src/genkidama.cpp
@@ -1,41 +1,72 @@
 #include "genkidama.h"
+#include "Lista.h"
+#include <iostream>
 
-void genkidama(int N,int T,vector<pair<int,int> > enemigos){
-	int genkidamas=0;
-	int i=0;
-	list<int> disparados;
-	while (i<N) {
-		int disparado=masLejanoQueMeMata(i,N,T,enemigos);
-	    i = masLejanoQueMato(disparado,N,T,enemigos) + 1;
 
-	    disparados.push_back(disparado+1); //el 0 de la lista es el enemigo 1 y asi sucesivamente
-	    genkidamas++;
-	}
+using namespace aed3;
+using std::cout;
 
-	printf("%d\n",genkidamas);
-	for(int i=0; i<genkidamas; i++){
-	  	printf("%d ",disparados.front());
-		disparados.pop_front();
-	}
+
+void genkidama(int cantSoldados,int T, Lista<Punto> lista){
+        Lista<Tupla> enemBarridos = Lista<Tupla>();
+        Lista<Punto>::Iterador iter = lista.CrearIt();
+        Lista<Punto>::Iterador iter2 = lista.CrearIt();
+        Lista<Tupla>::Iterador iterTupla = enemBarridos.CrearIt();
+        Tupla tuplaAux;
+        while (iter.HaySiguiente() == true){
+            if (iter2.Siguiente().y <= iter.Siguiente().y + T && iter2.Siguiente().x <= iter.Siguiente().x + T){
+                tuplaAux.x = iter2.Siguiente();
+                tuplaAux.y = iter.Siguiente();
+                tuplaAux.z = iter.Siguiente();
+                if (iterTupla.HayAnterior() == true) {
+                    iterTupla.EliminarAnterior();
+                }
+                enemBarridos.AgregarAtras(tuplaAux);
+            }
+            else{
+                tuplaAux.x = iter.Siguiente();
+                tuplaAux.y = iter.Siguiente();
+                tuplaAux.z = iter.Siguiente();
+                enemBarridos.AgregarAtras(tuplaAux);
+                iter2 = iter;
+                iterTupla.Avanzar();
+            }
+            iter.Avanzar();
+        }
+        Lista<Tupla>::Iterador iterTupla2 = enemBarridos.CrearItUlt();
+        Lista<Tupla>::Iterador iterTupla3 = enemBarridos.CrearItUlt();
+        Lista<Tupla> enemBarridos2 = Lista<Tupla>();
+        Lista<Tupla>::Iterador iterTupla5 = enemBarridos2.CrearIt();
+        while (iterTupla2.HayAnterior() == true){
+            if (iterTupla3.Anterior().z.y <= iterTupla2.Anterior().y.y + T && iterTupla3.Anterior().z.x <= iterTupla2.Anterior().y.x + T) {
+                tuplaAux.x = iterTupla2.Anterior().x;
+                tuplaAux.y = iterTupla2.Anterior().y;
+                tuplaAux.z = iterTupla3.Anterior().z;
+                if (iterTupla5.HayAnterior() == true) {
+                    iterTupla5.EliminarSiguiente();
+                }
+                enemBarridos2.AgregarAtras(tuplaAux);
+            }
+            else{
+                tuplaAux.x = iterTupla2.Anterior().x;
+                tuplaAux.y = iterTupla2.Anterior().y;
+                tuplaAux.z = iterTupla2.Anterior().z;
+                enemBarridos2.AgregarAtras(tuplaAux);
+                iterTupla3 = iterTupla2;
+                iterTupla5.Avanzar();
+            }
+            iterTupla2.Retroceder();
+        }
+
+        Lista<Tupla>::Iterador iterTupla4 = enemBarridos2.CrearIt();
+        while (iterTupla4.HaySiguiente() == true){
+            cout<<iterTupla4.Siguiente().y.z<<" ";
+
+
+            iterTupla4.Avanzar();
+        }
+
+
+
+
 }
-
-int masLejanoQueMeMata(int aMatar,int N, int T,vector<pair<int,int> > enemigos){
-    int j=aMatar+1;
-    while(loMata(j,aMatar,N,T,enemigos)){//me mata el siguiente?
-      j++;
-    }
-    return j-1;//devuelvo ultimo que me mata
-}
-
-int masLejanoQueMato(int disparado, int N,int T, vector<pair<int,int> > enemigos){
-  int j=disparado+1;
-  while(loMata(disparado,j,N,T,enemigos)){//mato al siguiente?
-    j++;
-  }
-  return j-1;//devuelvo ultimo que mato
-}
-
-bool loMata(int i,int j,int N,int T,vector<pair<int,int> > enemigos){//mata i al j?
-  return enemigos[i].first + T >= enemigos[j].first && enemigos[i].second + T >= enemigos[j].second;
-}
-
