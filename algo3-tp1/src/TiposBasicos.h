12cba8d4ce37d93938df01943947c6e5e0da8b0a
Sorongoku <gonzalorpg@gmail.com>
2016-03-26 01:22:13 -0300

    Added files via upload

---
+++ algo3-tp1/src/TiposBasicos.h
@@ -0,0 +1,37 @@
+
+#ifndef AED2_TIPOSBASICOS_H_INCLUDED
+#define AED2_TIPOSBASICOS_H_INCLUDED
+
+
+
+namespace aed3{
+    struct Punto{
+    int x;
+    int y;
+    int z;
+
+    bool operator == (const Punto& a) const{
+          return (a.x==this->x && a.y==this->y && a.z==this->z);
+        }
+        bool operator != (const Punto a) const {
+          return (a.x!=this->x || a.y!=this->y || a.z!=this->z);
+        }
+    };
+    struct Tupla{
+    Punto x;
+    Punto y;
+    Punto z;
+    bool operator == (const Tupla& a) const{
+        return (a.x==this->x && a.y==this->y && a.z==this->z);
+        }
+    bool operator != (const Tupla a) const {
+          return (a.x!=this->x || a.y!=this->y || a.z!=this->z);
+        }
+    };
+
+
+
+};
+
+
+#endif //AED2_TIPOSBASICOS_H_INCLUDED
